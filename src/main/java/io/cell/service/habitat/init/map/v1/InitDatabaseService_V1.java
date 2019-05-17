0eb9409187c0c8e334d4bee696321ac20e192b0a
Artem Batalov <artem.batalov.prof@gmail.com>
2019-05-17 12:19:05 +0300

    + Heroku config

--- src/main/java/io/cell/service/habitat/init/map/v1/InitDatabaseService_V1.java
+++ src/main/java/io/cell/service/habitat/init/map/v1/InitDatabaseService_V1.java
@@ -8,7 +8,6 @@ import io.cell.service.habitat.repositories.RegionRepository;
 import io.cell.service.habitat.services.CellFeaturesService;
 import io.cell.service.habitat.services.CellService;
 import io.cell.service.habitat.services.CellServiceImpl;
-import javafx.util.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
